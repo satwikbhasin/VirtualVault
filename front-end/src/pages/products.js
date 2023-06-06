2d449679bb4c1a3e675fd9dcc34a21dec1e6e625
satwikbhasin <satwikbhasin@gmail.com>
2023-06-06 00:45:12 -0400

    basic page setup

---
+++ front-end/src/pages/products.js
@@ -0,0 +1,11 @@
+import Container from "react-bootstrap/Container";
+
+function products() {
+  return (
+    <Container>
+      <h1 className="text-center">Products</h1>
+    </Container>
+  );
+}
+
+export default products;
