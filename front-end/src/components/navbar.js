f8b13b970465a778a4be37104ce444637a65427c
satwikbhasin <satwikbhasin@gmail.com>
2023-06-06 15:07:23 -0400

    add contact details

--- front-end/src/components/navbar.js
+++ front-end/src/components/navbar.js
@@ -1,20 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "../styling/navbar.css";
 
 function navbar() {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light p-3">
-  <a class="navbar-brand" href="/">Healthkare</a>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Contact</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/pages/products.js">Products</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+      <a class="navbar-brand" href="/">
+        Healthkare
+      </a>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="/pages/products.js">
+              Products
+            </a>
+          </li>
+        </ul>
+    </nav>
   );
 }
 
