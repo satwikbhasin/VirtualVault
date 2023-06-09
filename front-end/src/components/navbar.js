e2180fceab016fb65db5feb8b6141d147a8b9d12
satwikbhasin <satwikbhasin@gmail.com>
2023-06-08 20:37:37 -0400

    enhancements to navbar, styling & details page

--- front-end/src/components/navbar.js
+++ front-end/src/components/navbar.js
@@ -1,27 +1,41 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, NavItem } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import '../styling/navbar.css';
+import "../styling/text-styling.css";
 
 function navbar() {
   return (
-    <Navbar
-      className="navbar-expand-lg navbar-dark bg-dark p-3"
-    >
-      <NavLink className="navbar-brand" to="/">
-        Healthkare
-      </NavLink>
-      <NavItem>
-        <NavLink
-          className="text-light nav-link"
-          activeClassName="active"
-          exact
-          to="/products"
-        >
-          Products
+    <>
+      <Navbar className="navbar navbar-expand-lg navbar-dark bg-dark px-3 py-1">
+        <NavLink className="navbar-brand" to="/">
+          <h2>Healthkare</h2>
         </NavLink>
-      </NavItem>
-    </Navbar>
+      </Navbar>
+      <Navbar className="navbar-dark bg-secondary px-3 sm py-0">
+        <NavItem className="nav-tabs">
+          <NavLink
+            className="text-light"
+            activeClassName="active"
+            exact
+            to="/products"
+            style={{ textDecoration: "none" }}
+          >
+            <h6>Products</h6>
+          </NavLink>
+        </NavItem>
+        <NavItem className="nav-tabs m-3">
+          <NavLink
+            className="text-light"
+            activeClassName="active"
+            exact
+            to="/contact"
+            style={{ textDecoration: "none" }}
+          >
+            <h6>Contact</h6>
+          </NavLink>
+        </NavItem>
+      </Navbar>
+    </>
   );
 }
 
--- front-end/src/pages/products.js
+++ front-end/src/pages/products.js
@@ -1,12 +1,49 @@
 import React from "react";
-import ProductList from "../components/productListGenerator";
-import productData from "../data.json";
+import { Card, Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../styling/card.css";
+import { useState } from "react";
+import productMapInstance from "../services/productMap";
 
 const Products = () => {
+  const [hoveredCardIndex, setHoveredCardIndex] = useState(null);
+
+  const handleMouseEnter = (index) => {
+    setHoveredCardIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredCardIndex(null);
+  };
+
+  const productsMap = productMapInstance.getProductsFromMap();
+
   return (
-    <div>
-        <ProductList data={productData} />
-    </div>
+    <Container>
+      <Row className="pt-4 pb-4">
+        {Array.from(productsMap.values()).map((item) => (
+          <Col md={4} lg={4} xl={4} key={item.product_id}>
+            <Link to={`/product/${item.product_id}`} class="card-name">
+              <Card
+                className={`m-3 card ${
+                  hoveredCardIndex === item.product_id ? "enlarged" : ""
+                }`}
+                onMouseEnter={() => handleMouseEnter(item.product_id)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <Card.Body>
+                  <Card.Img src={item.image} className="card-image" />
+                  <Card.Title className="pt-2" class="font-product-name">
+                    {item.name}
+                  </Card.Title>
+                  {/* <Card.Text className="m-2">{item.description}</Card.Text> */}
+                </Card.Body>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
