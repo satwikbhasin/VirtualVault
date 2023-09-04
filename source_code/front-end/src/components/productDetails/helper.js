c6b3e271a88ced3ef1e48cb3dbadda877bc6ed97
satwikbhasin <satwikbhasin@gmail.com>
2023-09-04 19:35:20 -0400

    update links

--- source_code/front-end/src/components/productDetails/helper.js
+++ source_code/front-end/src/components/productDetails/helper.js
@@ -1,7 +1,7 @@
 import { saveAs } from "file-saver";
 import { Document, Packer, Paragraph, TextRun } from "docx";
 import jsPDF from "jspdf";
-import Backend from "../assets/backendLink";
+import Backend from "../../assets/BackendLink.js";
 
 export const handleGoBack = () => {
   if (window.location.pathname === "/admin/inventory") {
