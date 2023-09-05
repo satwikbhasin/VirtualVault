90f8c51de0333884185199f75c44534e4f9034d5
satwikbhasin <satwikbhasin@gmail.com>
2023-09-04 23:20:53 -0400

    bakcend_link_fix

--- source_code/front-end/src/services/contactAPIs.js
+++ source_code/front-end/src/services/contactAPIs.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import Backend from "../assets/BackendLink.js";
+import Backend from "../assets/backendLink.js";
 
 export const getContact = () => {
   return Axios.get(Backend + "/contactDetails/getContactDetails");
