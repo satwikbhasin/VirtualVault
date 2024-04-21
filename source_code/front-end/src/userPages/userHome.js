62d48ba706859c67f0f73157db359b6707f685fb
Satwik Bhasin <satwikbhasin@gmail.com>
2024-04-21 15:11:12 -0400

    home-image-changes

--- source_code/front-end/src/userPages/userHome.js
+++ source_code/front-end/src/userPages/userHome.js
@@ -9,7 +9,7 @@ function Home() {
       <UserNavbar />
       <div className="home-screen-watermark primary-bg full-screen-bg">
         <img
-          src={"https://storyset.com/illustration/science/amico"}
+          src={"https://i.ibb.co/2M9CYGq/Hospital-bed-cuate.png"}
           alt=""
           height={500}
           width={500}
