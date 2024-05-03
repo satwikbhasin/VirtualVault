3087af7fb9aed41b4b1447ac773cc9cd3892dd71
satwikbhasin <satwikbhasin@gmail.com>
2024-05-03 12:16:36 -0400

    s3_handled+keys_hidden

--- source_code/back-end/routes/s3Methods.js
+++ source_code/back-end/routes/s3Methods.js
@@ -10,33 +10,37 @@ const AWS = require("aws-sdk");
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region:  process.env.AWS_REGION,
+  region: process.env.AWS_REGION,
 });
 
 router.post(
   "/uploadImage/:mongoProductId",
   upload.single("image"),
   async (req, res) => {
-    const s3 = new AWS.S3();
+    // const s3 = new AWS.S3();
+    // const productImageFile = req.file;
+
+    // const fileData = fs.readFileSync(productImageFile.path);
+
+    // const params = {
+    //   Bucket: "healthkare",
+    //   Key: "product-images/" + req.params.mongoProductId + ".jpeg",
+    //   Body: fileData,
+    // };
+
+    // s3.upload(params)
+    //   .promise()
+    //   .then((data) => {
+    //     console.log(data.Location);
+    //     res.send(data.Location);
+    //     fs.unlinkSync(productImageFile.path);
+    //   })
+    //   .catch((error) => {
+    //     res.send("failed");
+    //   });
     const productImageFile = req.file;
-
-    const fileData = fs.readFileSync(productImageFile.path);
-
-    const params = {
-      Bucket: "healthkare",
-      Key: "product-images/" + req.params.mongoProductId + ".jpeg",
-      Body: fileData,
-    };
-
-    s3.upload(params)
-      .promise()
-      .then((data) => {
-        res.send(data.Location);
-        fs.unlinkSync(productImageFile.path);
-      })
-      .catch((error) => {
-        res.send(error);
-      });
+    fs.unlinkSync(productImageFile.path);
+    res.send("failed");
   }
 );
 
